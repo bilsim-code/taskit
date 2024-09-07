96fb8f4db616c6250f2fd0cd5f76d22e3413227a
Bil <bildadsimiyu6@gmail.com>
2024-09-07 16:11:24 +0300

    add edit form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Footer } from "./Components/Footer"
 import Navbar from "./Components/Navbar"
 import About from "./Pages/About"
+import EditItem from "./Pages/EditItem"
 import Home from "./Pages/Home"
 import { Route, Routes } from "react-router-dom"
 
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />} />
+        <Route path="/edit" element={<EditItem/>} />
       </Routes>
       <Footer/>
     </div>
--- client/src/Pages/EditItem.jsx
+++ client/src/Pages/EditItem.jsx
@@ -1,7 +1,24 @@
 
 const EditItem = () => {
   return (
-    <div>EditItem</div>
+    <form className="max-w-4xl mx-auto p-4">
+        <input type="text" name="title" placeholder="Task Title" required />
+        <textarea name="description" id="description" placeholder="Task Description"></textarea>
+        <select name="priority" id="priority" required>
+            <option value="">** Choose Priority **</option>
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+        </select>
+        <select name="status" id="" required>
+            <option value="">** Choose priority of the task **</option>
+            <option value="pending">Pending</option>
+            <option value="inProgress">In-Progress</option>
+            <option value="completed">Completed</option>
+        </select>
+        <input type="date" name="dueDate" id="dueDate" />
+        <button type="submit">Update Task</button>
+    </form>
   )
 }
 

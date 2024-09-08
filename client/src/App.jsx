d5fa1505a90e28389f0b80e834e50aab397473ab
Bil <bildadsimiyu6@gmail.com>
2024-09-08 16:53:07 +0300

    switch routes so that login page is the default

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Footer } from "./Components/Footer"
 import Navbar from "./Components/Navbar"
 import About from "./Pages/About"
 import AddTask from "./Pages/AddTask"
+import Auth from "./Pages/Auth"
 import EditItem from "./Pages/EditItem"
 import Home from "./Pages/Home"
 import { Route, Routes } from "react-router-dom"
@@ -11,7 +12,8 @@ const App = () => {
     <div className="">
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Auth />}/>
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/edit" element={<EditItem/>} />
         <Route path="/add-task" element={<AddTask/>} />

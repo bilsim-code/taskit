ad36ad10198e5fa02f1417f793997954dec32dc9
Bil <bildadsimiyu6@gmail.com>
2024-09-08 11:53:13 +0300

    create userModel

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const userRoute = require('./routes/userRoute');
+//END OF IMPORTS
+
+//routes
+app.use("/api/auth", userRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`)

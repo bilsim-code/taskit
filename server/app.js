3b0593a5dcb7f2c98ac3964fccfaaa4ab3e3f30f
Bil <bildadsimiyu6@gmail.com>
2024-09-08 12:27:00 +0300

    .gitignore file

--- server/app.js
+++ server/app.js
@@ -7,8 +7,11 @@ mongoose.connect(process.env.MONGODBURI)
 .then(() => console.log(`Database connected: ${mongoose.connection.host}`))
 //END OF IMPORTS
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 //routes
-app.use("/api/auth", userRoute);
+app.use("/user/auth", userRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`)
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,4 +2,9 @@ const express = require('express');
 const route = express.Router();
 const userModel = require('../models/userModel');
 
+//GET LOGIN
+route.get('/login', async(req, res) => {
+    res.send("Hi, it's me")
+})
+
 module.exports = route;

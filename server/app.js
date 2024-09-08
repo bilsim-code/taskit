99a291a063b888e0d7dde495db2da798d1f90277
Bil <bildadsimiyu6@gmail.com>
2024-09-08 15:40:01 +0300

    send response on successful registration

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 //routes
-app.use("/user/auth", userRoute);
+app.use("/auth", userRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`)

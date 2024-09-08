eac059a989e74c0903372ac534ec947bc1f29af3
Bil <bildadsimiyu6@gmail.com>
2024-09-08 12:43:26 +0300

    add bcrypt  and jwt to userRoute

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const route = express.Router();
 const userModel = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jwt');
 
 //GET LOGIN
 route.get('/login', async(req, res) => {

a6e9aa7ad933aaf4e9f162e377cbf40093ff795a
Bil <bildadsimiyu6@gmail.com>
2024-09-09 11:31:37 +0300

    setup authMiddleware

--- server/auth/authMiddleware.js
+++ server/auth/authMiddleware.js
@@ -2,6 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = async(req, res, next) => {
     try {
+        const {token} = req.headers;
+        if(!token) {
+            return res.json({success: false, message: "Authentication failed"});
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.headers.userId = decoded.userId
         
     } catch (error) {
         

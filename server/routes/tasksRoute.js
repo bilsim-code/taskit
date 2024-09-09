4c6c73cd8dfa0006f2a9b904a2467a576b390f5d
Bil <bildadsimiyu6@gmail.com>
2024-09-09 13:18:24 +0300

    add tasks route

--- server/routes/tasksRoute.js
+++ server/routes/tasksRoute.js
@@ -1,35 +1,48 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
-const tasksModel = require('../models/taskModel');
-const authMiddleware = require('../auth/authMiddleware');
+const tasksModel = require("../models/taskModel");
+const authMiddleware = require("../auth/authMiddleware");
 
 //POST /api/tasks/add
-route.post('/add', authMiddleware, async(req, res) => {
-    try {
-        const {title, description, priority, status, dueDate} = req.body;
-        
-    } catch (error) {
-        
+route.post("/add", authMiddleware, async (req, res) => {
+  try {
+    const { title, description, priority, status, dueDate } = req.body;
+    if (
+      title.trim() === "" ||
+      description.trim() === "" ||
+      priority.trim() === "" ||
+      status.trim() === "" ||
+      dueDate.trim() === ""
+    ) {
+      return res.json({ success: false, message: "Fill in all the fields" });
     }
-})
+
+    const data = await tasksModel.create({
+        title,
+        description,
+        priority,
+        status,
+        dueDate,
+    })
+
+    res.json({success: true, data});
+
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message: "Error Creating task"})
+  }
+});
 
 //POST /api/tasks/edit/:id
-route.post('/edit/:id', authMiddleware, async(req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-})
+route.post("/edit/:id", authMiddleware, async (req, res) => {
+  try {
+  } catch (error) {}
+});
 
 //POST /api/tasks/delete/:id
-route.post('/delete/:id', authMiddleware, async(req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-})
+route.post("/delete/:id", authMiddleware, async (req, res) => {
+  try {
+  } catch (error) {}
+});
 
-
-module.exports = tasksModel;
+module.exports = route;

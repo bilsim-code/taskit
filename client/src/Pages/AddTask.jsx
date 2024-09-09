7bc7baa53621acd1d8af1f16f71e857986a18b30
Bil <bildadsimiyu6@gmail.com>
2024-09-09 17:11:53 +0300

    add controlled inputs for the add task form

--- client/src/Pages/AddTask.jsx
+++ client/src/Pages/AddTask.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import {  useState } from "react"
 
 const AddTask = () => {
   const [data, setData ] = useState({
@@ -8,26 +8,37 @@ const AddTask = () => {
     status: "",
     dueDate: "",
   })
+
+  const handleOnchange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setData(prev => ({...prev, [name]: value})); 
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+  }
+
   return (
-    <form className="max-w-4xl mx-auto p-4">
+    <form className="max-w-4xl mx-auto p-4" onSubmit={handleSubmit}>
     <h2 className="text-center uppercase text-2xl font-medium">Add Task</h2>
     <hr className="mb-10 h-[1px] w-[20%] mx-auto border-0 bg-slate-400"/>
-    <input type="text" name="title" placeholder="Task Title" className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" required />
-    <textarea name="description" id="description" placeholder="Task Description" className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3"></textarea>
-    <select name="priority" id="priority" className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" required>
+    <input type="text" name="title" placeholder="Task Title" className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" required onChange={handleOnchange} value={data.title} />
+    <textarea name="description" id="description" placeholder="Task Description" className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" onChange={handleOnchange} value={data.description}></textarea>
+    <select name="priority" id="priority" className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" required onChange={handleOnchange} value={data.priority} >
         <option value="">** Choose Priority **</option>
         <option value="low">Low</option>
         <option value="medium">Medium</option>
         <option value="high">High</option>
     </select>
-    <select name="status" id="" className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" required>
+    <select name="status" id="" className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" required onChange={handleOnchange} value={data.status}>
         <option value="">** Choose status of the task **</option>
         <option value="pending">Pending</option>
         <option value="inProgress">In-Progress</option>
         <option value="completed">Completed</option>
     </select>
-    <input type="date" name="dueDate" id="dueDate" className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" />
-    <button type="submit" className="rounded p-1 w-1/2 max-nav-xs:w-full mx-auto bg-sky-300 ring-2 ring-slate-600 mt-6">Add Task</button>
+    <input type="date" name="dueDate" id="dueDate" className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" onChange={handleOnchange} value={data.dueDate}/>
+    <button type="submit" className="rounded p-1 w-1/2 max-nav-xs:w-full mx-auto bg-sky-300 ring-2  ring-slate-600 mt-6">Add Task</button>
 </form>
   )
 }

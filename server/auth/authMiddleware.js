41d4a7d3eb32cf0adeba68a2f11be01469a88461
Bil <bildadsimiyu6@gmail.com>
2024-09-09 11:42:29 +0300

    authMiddlleware error message

--- server/auth/authMiddleware.js
+++ server/auth/authMiddleware.js
@@ -4,14 +4,15 @@ const authMiddleware = async(req, res, next) => {
     try {
         const {token} = req.headers;
         if(!token) {
-            return res.json({success: false, message: "Authentication failed"});
+            return res.json({success: false, message: "Authentication failed!"});
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.body.userId = decoded.userId
         
     } catch (error) {
-        
+        console.log(error);
+        res.json({success: false, message: "Authentication error"});
     }
 }
 

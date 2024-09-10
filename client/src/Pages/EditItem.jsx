616140ee938491977f1fd1d12bfac6f38efd5abd
Bil <bildadsimiyu6@gmail.com>
2024-09-10 19:19:23 +0300

    fetch tasks with id

--- client/src/Pages/EditItem.jsx
+++ client/src/Pages/EditItem.jsx
@@ -2,12 +2,12 @@ import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../Components/AppContext";
 import { useNavigate, useParams } from "react-router-dom";
-import {toast} from 'react-toastify' 
+import { toast } from "react-toastify";
 
 const EditItem = () => {
-  const {url, token,fetchTaskList} = useContext(AppContext);
-  const navigate = useNavigate()
-  const {id} = useParams();
+  const { url, token, fetchTaskList } = useContext(AppContext);
+  const navigate = useNavigate();
+  const { id } = useParams();
   const [data, setData] = useState({
     title: "",
     description: "",
@@ -18,42 +18,44 @@ const EditItem = () => {
 
   //fetch the task by id to prefill the form
   useEffect(() => {
-    const fetchTask = async() => {
+    const fetchTask = async () => {
       try {
-        const response = await axios.get(`${url}/api/tasks/${id}`,{ headers: {token}});
-        if(response.data.success) {
+        const response = await axios.get(`${url}/api/tasks/${id}`, {
+          headers: { token },
+        });
+        if (response.data.success) {
           setData(response.data.data);
         }
-       
       } catch (error) {
         console.log("Error fetching task", error);
       }
     };
     fetchTask();
-  }, [id, url,token])
+  }, [id, url, token]);
 
   const handleOnChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    setData(prevData => ({...prevData, [name]: value}))
-  }
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${url}/api/tasks/edit/${id}`, data, {headers: {token}});
-      if(response.data.success) {
+      const response = await axios.post(`${url}/api/tasks/edit/${id}`, data, {
+        headers: { token },
+      });
+      if (response.data.success) {
         toast.success(response.data.message);
-        navigate('/home');
-        fetchTaskList(token)
-      }
-      else {
+        navigate("/home");
+        fetchTaskList(token);
+      } else {
         toast.error(response.data.message);
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
     <form className="max-w-4xl mx-auto p-4" onSubmit={handleSubmit}>
@@ -64,19 +66,26 @@ const EditItem = () => {
         name="title"
         placeholder="Task Title"
         className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3"
-        required onChange={handleOnChange} value={data.title}
+        required
+        onChange={handleOnChange}
+        value={data.title}
       />
       <textarea
         name="description"
         id="description"
         placeholder="Task Description"
         className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3"
-        onChange={handleOnChange} value={data.description}
-      >{data.description}</textarea>
+        onChange={handleOnChange}
+        value={data.description}
+      >
+        {data.description}
+      </textarea>
       <select
         name="priority"
         id="priority"
-        className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" onChange={handleOnChange} value={data.priority}
+        className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3"
+        onChange={handleOnChange}
+        value={data.priority}
         required
       >
         <option value="">** Choose Priority **</option>
@@ -87,7 +96,9 @@ const EditItem = () => {
       <select
         name="status"
         id=""
-        className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" onChange={handleOnChange} value={data.status}
+        className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3"
+        onChange={handleOnChange}
+        value={data.status}
         required
       >
         <option value="">** Choose status of the task **</option>
@@ -95,12 +106,14 @@ const EditItem = () => {
         <option value="In-Progress">In-Progress</option>
         <option value="Completed">Completed</option>
       </select>
-      <label htmlFor="dueDate">Due Date</label> 
+      <label htmlFor="dueDate">Due Date</label>
       <input
         type="date"
         name="dueDate"
         id="dueDate"
-        className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3" onChange={handleOnChange} value={data.dueDate}
+        className="border border-black focus:outline-slate-800 rounded p-2 w-full mb-3"
+        onChange={handleOnChange}
+        value={data.dueDate}
       />
       <button
         type="submit"
--- client/src/Pages/Task.jsx
+++ client/src/Pages/Task.jsx
@@ -1,7 +1,32 @@
+import { useContext, useEffect, useState } from "react"
+import { AppContext } from "../Components/AppContext";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const Task = () => {
+    const [task, setTask] = useState({});
+    const {url, token, fetchTaskList} = useContext(AppContext);
+    const id = useParams();
+
+    useEffect(() => {
+        const fetchTask = async () => {
+          try {
+            const response = await axios.get(`${url}/api/tasks/${id}`, {
+              headers: { token },
+            });
+            if (response.data.success) {
+              setTask(response.data.data);
+            }
+          } catch (error) {
+            console.log("Error fetching task", error);
+          }
+        };
+        fetchTask();
+      }, [id, url, token]);
   return (
-    <div>Task</div>
+    <div>
+
+    </div>
   )
 }
 
